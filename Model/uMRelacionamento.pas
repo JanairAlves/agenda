b07a698bb54e3d2da8a75d2811c0afa488e769cf
Janair Alves <janairalves@gmail.com>
2025-06-01 23:35:24 -0300

    Módulo salvar contato.

---
+++ Model/uMRelacionamento.pas
@@ -0,0 +1,15 @@
+unit uMRelacionamento;
+
+interface
+
+type
+  TMRelacionamento = (
+    [EnumDescription('Outros')] tOutros,
+    [EnumDescription('Família')] tFamilia,
+    [EnumDescription('Trabalho')] tTrabalho,
+    [EnumDescription('Amigo')] tAmigo
+    );
+
+implementation
+
+end.
